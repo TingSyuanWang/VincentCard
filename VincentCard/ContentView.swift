034981122ee3806fc61c3b266dddfcc4c89a5e55
TingSyuanWang <vincent@fishboneapps.com>
2023-03-18 17:38:17 -0400

    feat: add cards info and adapt dark mode
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

--- VincentCard/ContentView.swift
+++ VincentCard/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("vincent")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Vincent Wang")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+1 123 456 7890", imageName: "phone.fill")
+                InfoView(text: "vincent@fsihboneapps.com", imageName: "envelope.fill")
+            }
         }
-        .padding()
     }
 }
 
---
+++ VincentCard/InfoView.swift
@@ -0,0 +1,32 @@
+//
+//  InfoView.swift
+//  VincentCard
+//
+//  Created by Ting-Hsuan Wang on 3/18/23.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .frame(height: 50)
+            .foregroundColor(Color("Info Color"))
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
