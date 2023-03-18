6bd409b7af6656f940e8bee5a051e9123cdf78ef
TingSyuanWang <vincent@fishboneapps.com>
2023-03-18 16:58:31 -0400

    Initial Commit

---
+++ VincentCard/VincentCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VincentCardApp.swift
+//  VincentCard
+//
+//  Created by Ting-Hsuan Wang on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct VincentCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
